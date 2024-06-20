60bf12aa4f904e7724271f7ec7adf21f2622d0c3
Eragon67360 <thomas-moser@orange.fr>
2024-06-20 14:03:24 +0200

    style(page.tsx): improve styling and layout of the Home component feat(fonts): add new font files for 'Impact Regular' and 'Impacted Regular' feat(globals.css): import 'fonts.css' to include font-face declarations feat(tailwind.config.js): add 'Impact' font family to the project for styling purposes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-     
+    <section className="w-screen h-screen bg-home bg-cover bg-no-repeat relative uppercase">
+      <h1 className="absolute top-8 text-4xl text-white w-full text-center font-[900]">Taylor Swift</h1>
+
+      <div className="absolute top-[266px] left-[230px] w-[94px]">
+        <p className="font-impact text-xl text-white">Who is taylor swift anyway? <span className="text-[#F00]">EW</span></p>
+      </div>
+
+      <div className="absolute top-[444px] right-[416px] w-[94px]">
+        <p className="font-impact text-xl text-white">This&nbsp;<br /><span className="text-[#F00]">is not</span>&nbsp;<br />Taylor&apos;s version</p>
+      </div>
+
+      <div className="absolute bottom-[306px] left-[130px] w-[94px]">
+        <p className="font-impact text-xl text-white"><span className="text-[#F00]">A lot</span>&nbsp;<br />going on at the moment</p>
+      </div>
+
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,12 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-geist-mono)"],
+        impact: ['Impact', 'sans-serif'],
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(180deg, rgba(217, 217, 217, 0.00) 0%, rgba(79, 79, 79, 0.58) 36.4%, #000 100%)',
         'tours':'url(\'/img/eras.jpg\')',
+        'home':'url(\'/img/bg_ts.jpg\')',
       },
     },
   },
