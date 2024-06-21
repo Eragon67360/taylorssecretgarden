5cfef9fa42979bd9d40f5536eb2fc47702644a3a
Eragon67360 <thomas-moser@orange.fr>
2024-06-21 10:30:18 +0200

    style(page.tsx): improve styling for Home component for better readability and responsiveness style(navbar.tsx): enhance styling for Navbar component to improve layout and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 
 export default function Home() {
   return (
-    <section className="w-screen h-screen bg-home bg-cover bg-no-repeat relative uppercase">
-      <h1 className="absolute top-8 text-4xl text-white w-full text-center font-[900]">Taylor Swift</h1>
+    <section className="w-screen h-screen bg-home bg-cover bg-no-repeat bg-center relative uppercase transition-all duration-300 pb-[120px] md:pb-[180px] lg:pb-[240px] xl:pb-[300px]">
+      <h1 className="absolute top-8 text-xl md:text-2xl lg:text-3xl xl:text-4xl text-white w-full text-center font-[900] transition-all duration-300">Taylor Swift</h1>
 
-      <div className="absolute top-[266px] left-[230px] w-[94px]">
-        <p className="font-impact text-xl text-white">Who is taylor swift anyway? <span className="text-[#F00]">EW</span></p>
+      <div className="absolute bg-black/40 top-[108px] left-[58px] md:top-[144px] md:left-[118px] lg:top-[204px] lg:left-[178px] xl:top-[266px] xl:left-[230px] w-[74px] md:w-[84px] lg:w-[94px] transition-all duration-300">
+        <p className="font-impact text-base md:text-lg lg:text-xl text-white">Who is taylor swift anyway? <span className="text-[#F00]">EW</span></p>
       </div>
 
-      <div className="absolute top-[444px] right-[416px] w-[94px]">
-        <p className="font-impact text-xl text-white">This&nbsp;<br /><span className="text-[#F00]">is not</span>&nbsp;<br />Taylor&apos;s version</p>
+      <div className="absolute bg-black/40 top-[264px] right-[56px] md:top-[324px] md:right-[176px] lg:top-[384px] lg:right-[296px] xl:top-[444px] xl:right-[416px] w-[74px] md:w-[84px] lg:w-[94px] transition-all duration-300">
+        <p className="font-impact text-base md:text-lg lg:text-xl text-white">This&nbsp;<br /><span className="text-[#F00]">is not</span>&nbsp;<br />Taylor&apos;s version</p>
       </div>
 
-      <div className="absolute bottom-[306px] left-[130px] w-[94px]">
-        <p className="font-impact text-xl text-white"><span className="text-[#F00]">A lot</span>&nbsp;<br />going on at the moment</p>
+      <div className="absolute bottom-[140px] left-[24px] md:bottom-[186px] md:left-[50px] lg:bottom-[246px] lg:left-[90px] xl:bottom-[306px] xl:left-[130px] w-[74px] md:w-[84px] lg:w-[94px] transition-all duration-300">
+        <p className="font-impact text-base md:text-lg lg:text-xl text-white"><span className="text-[#F00]">A lot</span>&nbsp;<br />going on at the moment</p>
       </div>
 
     </section>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,13 +15,13 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed z-50 bottom-0 bg-custom-gradient w-[100dvw] h-[300px] flex justify-center items-end pb-14 overflow-x-hidden">
-        <div className="w-fit h-12 px-8 py-3 flex justify-center items-center rounded-full bg-black text-white uppercase">
-          <ul className="flex items-center justify-center relative gap-8">
+      <div className="fixed z-50 bottom-0 bg-custom-gradient w-[100dvw] h-[80px] md:h-[180px] lg:h-[240px] xl:h-[300px] flex justify-center items-end pb-4 md:pb-8 lg:pb-12 xl:pb-14 overflow-x-hidden">
+        <div className="w-fit  px-4 md:px-5 lg:px-6 xl:px-8 py-3 md:py-2 lg:py-3 flex justify-center items-center rounded-full bg-black text-white uppercase">
+          <ul className="flex items-center justify-center relative gap-4 md:gap-5 lg:gap-6 xl:gap-8">
             {Menus.map((menu, i) => (
               <li key={i} className="">
                 <Link
-                  className="flex flex-col text-center cursor-pointer"
+                  className="flex flex-col text-center cursor-pointer text-xs md:text-base"
                   onClick={() => setActive(i)}
                   href={menu.path}>
                   <span
