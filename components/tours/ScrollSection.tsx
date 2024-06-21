1fc4f1e33ec17c30e319810817e7cbfd412f5987
Eragon67360 <thomas-moser@orange.fr>
2024-06-21 11:47:06 +0200

    feat(ScrollSection.tsx): refactor ScrollSection component to use data from tours.json feat(tours.json): add JSON file with tour data for ScrollSection component

--- components/tours/ScrollSection.tsx
+++ components/tours/ScrollSection.tsx
@@ -5,24 +5,43 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Image from 'next/image';
 import { VideoContext } from "@/context/VideoContext";
 import CldImage from "../ui/CldImageWrapper";
+import tours from '@/public/json/tours.json'
 
-const images = [
-    'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/eras',
-    'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/reputation',
-    'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/1989',
-    'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
-    'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/speak_now',
-    'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/fearless',
-];
+// const images = [
+//     'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/eras',
+//     'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/reputation',
+//     'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/1989',
+//     'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
+//     'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/speak_now',
+//     'https://res.cloudinary.com/dluezegi8/image/upload/f_auto,q_auto/v1/images/upload/taylorssecretgarden/tours/fearless',
+// ];
 
-const videos = [
-    'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/eras',
-    'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/reputation',
-    'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/nineteeneightynine',
-    'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
-    'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
-    'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
-];
+// const videos = [
+//     'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/eras',
+//     'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/reputation',
+//     'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/nineteeneightynine',
+//     'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
+//     'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
+//     'https://res.cloudinary.com/dluezegi8/video/upload/f_auto:video,q_auto/v1/images/upload/taylorssecretgarden/tours/red',
+// ];
+
+// const tours = [
+//     'The Eras Tour',
+//     'Reputation Tour',
+//     '1989 Tour',
+//     'The Red Tour',
+//     'Speak Now World Tour',
+//     'Fearless Tour',
+// ];
+
+// const dates = [
+//     '2023-2024',
+//     '2018',
+//     '2015',
+//     '2013-2014',
+//     '2011-2012',
+//     '2009-2010',
+// ];
 
 const ScrollSection = () => {
     const sectionRef = useRef<HTMLDivElement | null>(null);
@@ -35,7 +54,7 @@ const ScrollSection = () => {
 
     const imageWidth = 280;
     const spaceBetween = 20;
-    const totalWidth = images.length * imageWidth + (images.length - 1) * spaceBetween;
+    const totalWidth = tours.length * imageWidth + (tours.length - 1) * spaceBetween;
 
     const updateTranslateX = () => {
         if (sectionRef.current && triggerRef.current) {
@@ -63,14 +82,14 @@ const ScrollSection = () => {
                         scrub: 0,
                         pin: true,
                         snap: {
-                            snapTo: 1 / images.length,
+                            snapTo: 1 / tours.length,
                             duration: 1,
                             ease: "power1.inOut",
                         },
                         onUpdate: self => {
                             const progress = self.progress;
-                            const activeIndex = Math.floor(progress * images.length);
-                            setVideoSrc(videos[activeIndex]);
+                            const activeIndex = Math.floor(progress * tours.length);
+                            setVideoSrc(tours[activeIndex].videoUrl);
                         },
                     },
                 }
@@ -81,7 +100,7 @@ const ScrollSection = () => {
                 setScrollTriggerInstance(newScrollTrigger);
             }
 
-            images.forEach((image, index) => {
+            tours.forEach((image, index) => {
                 if (sectionRef.current && sectionRef.current.children[index]) {
                     const element = sectionRef.current.children[index].querySelector('img');
                     if (element) {
@@ -120,13 +139,34 @@ const ScrollSection = () => {
         <>
             <section className="overflow-hidden z-10">
                 <div ref={triggerRef} className="overflow-hidden">
-                    <div ref={sectionRef} className={`h-screen flex relative text-white font-bold text-2xl overflow-hidden text-ellipsis`} style={{ width: totalWidth, gap: spaceBetween }}>
-                        {images.map((image, index) => (
-                            <div key={index} className="h-screen py-44 flex items-end justify-center snap-start">
-                                <CldImage src={image} alt={`Tour ${index + 1}`} className="image w-[280px] h-[400px] object-cover rounded-2xl" width={280} height={400}/>
-                                {/* <Image src={image} alt={`Tour ${index + 1}`} className="image w-[280px] h-[400px] object-cover rounded-2xl" width={280} height={400} /> */}
+                    <div ref={sectionRef} className="flex flex-col h-screen">
+                        <div className="bg-transparent h-full min-h-32 flex flex-col justify-end" style={{ width: totalWidth }}>
+                            <div className="bg-black/40 py-4 space-y-4">
+                                <div className="text-white font-bold text-2xl text-center uppercase flex justify-between">
+                                    {tours.map((tour, index) => (
+                                        <div className="w-full" style={{ maxWidth: imageWidth }}>{tour.tour} <br />{tour.date}</div>
+                                    ))}
+                                </div>
+                                <div className="relative h-2 mx-auto" style={{ width:totalWidth-imageWidth  }}>
+                                    <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full h-1 bg-white"></div>
+                                    {tours.map((tour, index) => (
+                                        <div
+                                            key={index}
+                                            className="absolute top-1/2 transform -translate-y-1/2 bg-white rounded-full"
+                                            style={{ width: '20px', height: '20px', left: `calc(${index} * (100% / ${tours.length - 1}))` }}
+                                        ></div>
+                                    ))}
+                                </div>
                             </div>
-                        ))}
+                        </div>
+                        <div className={`h-full flex relative text-white font-bold text-2xl overflow-hidden text-ellipsis`} style={{ width: totalWidth, gap: spaceBetween }}>
+                            {tours.map((tour, index) => (
+                                <div key={index} className="h-full py-44 flex items-end justify-center snap-start">
+                                    <CldImage src={tour.imageUrl} alt={`Tour ${index + 1}`} className="image w-[280px] h-[400px] object-cover rounded-2xl" width={280} height={400} />
+                                </div>
+                            ))}
+                        </div>
+
                     </div>
                 </div>
             </section>
