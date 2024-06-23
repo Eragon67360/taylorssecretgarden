19def688d182ae78d9eab3b4a90267696bc60a55
Eragon67360 <thomas-moser@orange.fr>
2024-06-23 20:25:45 +0200

    chore(account-form.tsx): remove unused account-form component and page.tsx style(layout.tsx): update padding-bottom values for better layout spacing

--- app/forum/layout.tsx
+++ app/forum/layout.tsx
@@ -4,7 +4,7 @@ export default function PricingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center">      
+    <section className="flex flex-col items-center justify-center pb-[80px] md:pb-[180px] lg:pb-[240px] xl:pb-[300px]">      
         {children}
     </section>
   );
